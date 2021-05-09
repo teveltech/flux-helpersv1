2c14bb0400659cd7f05390df5ee327ba5506652f
rotem <rotem.g@tevel-tech.com>
2021-05-09 10:54:14 +0300

    add update & patch to git repo resource

--- clientset/gitrepository/gitrepositories.go
+++ clientset/gitrepository/gitrepositories.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
@@ -13,6 +14,8 @@ type GitRepositoryInterface interface {
 	List(opts metav1.ListOptions) (*sourcev1.GitRepositoryList, error)
 	Get(name string, options metav1.GetOptions) (*sourcev1.GitRepository, error)
 	Create(*sourcev1.GitRepository) (*sourcev1.GitRepository, error)
+	Update(gitRepo *sourcev1.GitRepository, opts metav1.UpdateOptions) (result *sourcev1.GitRepository, err error)
+	Patch(name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (result *sourcev1.GitRepository, err error)
 	Watch(opts metav1.ListOptions) (watch.Interface, error)
 	// ...
 }
@@ -60,6 +63,36 @@ func (c *gitRepositoryClient) Create(gitRepository *sourcev1.GitRepository) (*so
 		Into(&result)
 
 	return &result, err
+}
+
+// Update takes the representation of a gitRepo and updates it.
+// Returns the server's representation of the gitRepo, and an error, if there is any.
+func (c *gitRepositoryClient) Update(gitRepo *sourcev1.GitRepository, opts metav1.UpdateOptions) (result *sourcev1.GitRepository, err error) {
+	result = &sourcev1.GitRepository{}
+	err = c.restClient.Put().
+		Namespace(c.ns).
+		Resource("gitrepositories").
+		Name(gitRepo.Name).
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Body(gitRepo).
+		Do(context.TODO()).
+		Into(result)
+	return
+}
+
+// Patch applies the patch and returns the patched configMap.
+func (c *gitRepositoryClient) Patch(name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (result *sourcev1.GitRepository, err error) {
+	result = &sourcev1.GitRepository{}
+	err = c.restClient.Patch(pt).
+		Namespace(c.ns).
+		Resource("gitrepositories").
+		Name(name).
+		SubResource(subresources...).
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Body(data).
+		Do(context.TODO()).
+		Into(result)
+	return
 }
 
 func (c *gitRepositoryClient) Watch(opts metav1.ListOptions) (watch.Interface, error) {
